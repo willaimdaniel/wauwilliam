c7ccf0504fe0b282f31082fdd104e80c77101cf0
willaimdaniel <65925462+willaimdaniel@users.noreply.github.com>
2020-05-25 20:42:20 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,55 @@
+;
+const CACHE_NAME = 'v1_cache_Imperplus_wao',
+  urlsToCache = [
+    './',
+    'http://fonts.googleapis.com/css?family=Open+Sans',
+    './style.css',
+    './script.js',
+    './img/ImperPlusWAO'
+  ]
+
+  self.addEventListener('install', e => {
+    e.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          return cache.addAll(urlsToCache)
+            .then(() => self.skipWaiting())
+        })
+        .catch(err => console.log('Falló registro de cache', err))
+    )
+  })
+
+  self.addEventListener('activate', e => {
+    const cacheWhitelist = [CACHE_NAME]
+  
+    e.waitUntil(
+      caches.keys()
+        .then(cacheNames => {
+          return Promise.all(
+            cacheNames.map(cacheName => {
+              //Eliminamos lo que ya no se necesita en cache
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName)
+              }
+            })
+          )
+        })
+        // Le indica al SW activar el cache actual
+        .then(() => self.clients.claim())
+    )
+  })
+
+  self.addEventListener('fetch', e => {
+    //Responder ya sea con el objeto en caché o continuar y buscar la url real
+    e.respondWith(
+      caches.match(e.request)
+        .then(res => {
+          if (res) {
+            //recuperar del cache
+            return res
+          }
+          //recuperar de la petición a la url
+          return fetch(e.request)
+        })
+    )
+  })
